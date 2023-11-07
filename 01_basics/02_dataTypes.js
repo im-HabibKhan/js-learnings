cc5f6fd62771d9db92e57288b1fec15517d2cec8
Muhammad Habib-ur-Rehman Khan <50040880+im-HabibKhan@users.noreply.github.com>
2023-11-07 08:14:17 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3+3) // error here: wwe are using node.js 
+                // in browser we would get 6
+
+let name = "habib";
+let age = 22
+let isLoggedIn = false;
+
+// -------- Premitive DataTypes -------- //
+// number => range: 2 raise to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => An Empty standalone value // it is a typeof Object
+// undefined => not defined or initialized yet
+// symbol => unique ( we will explore further in react)
+
+
+// ----- Non-Premitive DataTypes --------//
+// Object
